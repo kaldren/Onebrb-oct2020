9bef5e3870e57ee9c02a51f7b22c7aeee64e6101
Kaloyan Drenski <drenski666@gmail.com>
2020-10-17 18:43:38 +0300

    Added ef core

---
+++ src/Onebrb.Data/OnebrbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Onebrb.Data
+{
+    public class OnebrbContext : DbContext
+    {
+        public OnebrbContext(DbContextOptions<OnebrbContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
