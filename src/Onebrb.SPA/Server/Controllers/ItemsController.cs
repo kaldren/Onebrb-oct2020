0a53707346e0aa79228d23449976d4e2490da303
Kaloyan Drenski <drenski666@gmail.com>
2020-10-21 08:07:13 +0300

    SPA Items

--- src/Onebrb.SPA/Server/Controllers/ItemsController.cs
+++ src/Onebrb.SPA/Server/Controllers/ItemsController.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Onebrb.SPA.Server.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ItemsController : ControllerBase
     {
-        public IActionResult Index()
+        public ItemsController()
         {
-            return View();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(long itemId)
+        {
+            string apiUrl = "http://localhost:55917/api/items";
+            DataTable result = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    result = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+                }
+
+                return new JsonResult(result);
+            }
         }
     }
 }
