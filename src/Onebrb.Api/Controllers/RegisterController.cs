53d822991bf6f683b4bf30085287c24aceb8c596
Kaloyan Drenski <drenski666@gmail.com>
2020-11-04 07:55:01 +0200

    RegisterController - creating account

---
+++ src/Onebrb.Api/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Onebrb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RegisterController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<bool> PostAsync()
+        {
+            var user = new IdentityUser
+            {
+                UserName = "admin",
+                Email = "drenski666@gmail.com",
+            };
+
+            var result = await _userManager.CreateAsync(user, "Parola123-");
+
+            return result.Succeeded;
+        }
+    }
+}
