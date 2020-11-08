568a8724781b565e9e177a318d0c67a31dfc9db0
Kaloyan Drenski <drenski666@gmail.com>
2020-11-08 10:08:41 +0200

    Integrated MVC

---
+++ src/Onebrb.Api/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Onebrb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
