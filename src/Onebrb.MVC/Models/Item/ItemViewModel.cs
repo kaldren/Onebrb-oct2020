f16d4c3816ce487d073410d101e20a7f73afe374
Kaloyan Drenski <drenski666@gmail.com>
2020-11-21 17:27:06 +0200

    Added id to delete

--- src/Onebrb.MVC/Models/Item/ItemViewModel.cs
+++ src/Onebrb.MVC/Models/Item/ItemViewModel.cs
@@ -7,6 +7,7 @@ namespace Onebrb.MVC.Models.Item
 {
     public class ItemViewModel
     {
+        public long Id { get; set; }
         public decimal? Price { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
