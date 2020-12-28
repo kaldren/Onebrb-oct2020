b11695fa725e612b852ed0d16f85e50e6848c5c6
Kaloyan Drenski <drenski666@gmail.com>
2020-12-28 09:20:02 +0200

    Added like button and FA library

---
+++ src/Onebrb.MVC/Components/Items/ViewItem.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Onebrb.MVC.Constants;
+using Onebrb.MVC.Models.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onebrb.MVC.Components.Items
+{
+    public partial class ViewItem : ComponentBase
+    {
+        // Parameters
+        [Parameter]
+        public ItemViewModel Item { get; set; }
+
+        public string BtnLikeCss { get; set; } = $"{BootstrapCssConst.Btn} {BootstrapCssConst.BtnSuccess}";
+        public bool IsLikeBtnEnabled { get; set; }
+        public string LikeBtnText { get; set; } = "Like";
+    }
+}
